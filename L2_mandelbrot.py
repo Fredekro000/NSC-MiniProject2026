105131e3a009933c61ba7a44a69e7b1f10411fda
Fredekro000 <nschol22@student.aau.dk>
2026-02-19 14:24:32 +0100

    Improved mandelbrot 3.383 --> 1.451 seconds
    Integrating vectorization and numpy

--- L2_mandelbrot.py
+++ L2_mandelbrot.py
@@ -12,14 +12,42 @@ y_min = -1.5
 
 size = 1024
 
-# Grid
+# Grid function
 def grid():
     x = np.linspace(x_min, x_max, size)
     y = np.linspace(y_min, y_max, size)
     X, Y = np.meshgrid(x, y)
 
-    C = X + 1j + Y
+    C = X + 1j * Y
 
-    print (f" Shape : {C. shape }") 
-    print (f" Type : {C. dtype }") 
+    return C
 
+# Mandelbrot function
+def mandelbrot():
+    C = grid()
+
+    Z = np.zeros_like(C)
+    M = np.zeros(C.shape, dtype=int)
+
+    for _ in range(max_iter):
+        mask = np.abs(Z) <= bound
+        Z[mask] = Z[mask]**power + C[mask]
+        M[mask] += 1
+    
+    return M
+
+
+start = time.time()
+M = mandelbrot()
+elapsed = time.time() - start
+
+ax = plt.axes()
+plt.rc('text', usetex = True)
+ax.set_aspect('equal')
+graph = ax.pcolormesh(M, cmap='hot')
+plt.colorbar(graph)
+plt.xlabel("Real-Axis")
+plt.ylabel("Imaginary-Axis")
+plt.title(f"Computation took {elapsed:.3f} seconds")
+plt.gcf().set_size_inches(5,4)
+plt.show()
